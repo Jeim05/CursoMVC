3f4b2c7dceb4ac20a3cf915f09b3474857439b9b
Jeim05 <lopezvegajeimmy@gmail.com>
2024-07-30 23:39:11 -0600

    Creacion de vistas

--- CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -13,25 +13,11 @@ namespace CapaPresentacionAdmin.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Usuarios()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
-
-        public ActionResult Test()
-        {
-            ViewBag.Message = "Your test page.";
-
-            return View();
-        }
     }
 }
