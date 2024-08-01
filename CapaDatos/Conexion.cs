a04e20dcb9329d46f69a99fd14cebbf65ba8457f
Jeim05 <lopezvegajeimmy@gmail.com>
2024-07-31 22:15:19 -0600

    Agregar las referencias en las distintas capas.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string conexion = ConfigurationManager.ConnectionStrings["cadena"].ToString();  /* Ese nombre cadena proviene desde el Web.config, que es el nombre que le asignammos a la conexion */
+
+    }
+}
