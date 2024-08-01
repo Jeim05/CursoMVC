75a814191e43bf48752b4841c7a674b2e96550e1
Jeim05 <lopezvegajeimmy@gmail.com>
2024-07-31 22:48:31 -0600

    Llamar la lista en la capa de negocio

---
+++ CapaNegocio/CN_Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Usuarios
+    {
+        private CD_Usuarios objCapaDatso = new CD_Usuarios();
+        
+        public List<Usuario> Listar()
+        {
+            return objCapaDatso.Listar(); // Metodo que devuelve la lista con los usuarios registrados
+        }
+    }
+}
