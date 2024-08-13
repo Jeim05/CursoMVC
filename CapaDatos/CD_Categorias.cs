36f0ac881ef8be17c5b194c21ac98932ea13d491
Jeim05 <lopezvegajeimmy@gmail.com>
2024-08-12 20:56:45 -0600

    Capa datos Categoria

---
+++ CapaDatos/CD_Categorias.cs
@@ -0,0 +1,147 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaEntidad;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CD_Categorias
+    {
+        public List<Categoria> Listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.conexion))
+                {
+                    string query = "select IdCategoria, Descripcion, Activo from CATEGORIA";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text; // Se da este instruccion ya que el query es un texto simple
+                    oconexion.Open();
+
+
+                    // SqlDataReader hace la lectura y da el resultado de la ejecucion de la consulta
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lista.Add(
+                                new Categoria
+                                {
+                                    IdCategoria = Convert.ToInt32(reader["IdCategoria"]),
+                                    Descripcion = reader["Descripcion"].ToString(),
+                                    Activo = Convert.ToBoolean(reader["Activo"]),
+                                });
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                lista = new List<Categoria>();
+            }
+
+            return lista;
+        }
+
+        public int Registrar(Categoria obj, out string Mensaje)
+        {
+            int idAutogenerado = 0; // Se recibe el id del usuario al momento de hacer el registro
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.conexion))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarCategoria", conexion); // Hacemos referencia al prcedimiento almacenado creado
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output; // Parametros de salida 
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output; // Parametros de salida
+                    cmd.CommandType = CommandType.StoredProcedure; // Se indica que es un procedimiento almacenado
+
+                    conexion.Open();
+                    cmd.ExecuteNonQuery();
+                    idAutogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value); // Retornamos el parametro de salida del Procedimiento
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString(); // Se obtiene el mensaje del procedimiento almacenado
+                }
+            }
+            catch (Exception ex)
+            {
+                idAutogenerado = 0;
+                Mensaje = ex.Message;
+            }
+            return idAutogenerado;
+        }
+
+        public bool Editar(Categoria obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.conexion))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EditarCategoria", conexion); // Hacemos referencia al prcedimiento almacenado creado
+                    cmd.Parameters.AddWithValue("IdCategoria", obj.IdCategoria);
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output; // Parametros de salida 
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output; // Parametros de salida
+                    cmd.CommandType = CommandType.StoredProcedure; // Se indica que es un procedimiento almacenado
+
+                    conexion.Open(); // Se abre la conexión 
+                    cmd.ExecuteNonQuery(); // Se ejecuta el procedimiento
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value); // Retornamos el parametro de salida del Procedimiento
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString(); // Se obtiene el mensaje del procedimiento almacenado
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.conexion))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EliminarCategoria", conexion); // Hacemos referencia al prcedimiento almacenado creado
+                    cmd.Parameters.AddWithValue("IdCategoria", id);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output; // Parametros de salida 
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output; // Parametros de salida
+                    cmd.CommandType = CommandType.StoredProcedure; // Se indica que es un procedimiento almacenado
+
+                    conexion.Open(); // Se abre la conexión 
+                    cmd.ExecuteNonQuery(); // Se ejecuta el procedimiento
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value); // Retornamos el parametro de salida del Procedimiento
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString(); // Se obtiene el mensaje del procedimiento almacenado
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+
+
+    }
+}
