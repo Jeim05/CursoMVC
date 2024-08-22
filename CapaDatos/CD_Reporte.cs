3b98483f49f3cb422ebec8b5eeba7df1e139b8ae
Jeim05 <lopezvegajeimmy@gmail.com>
2024-08-21 21:48:56 -0600

    Funcionalidades para ver reportes dashboard

---
+++ CapaDatos/CD_Reporte.cs
@@ -0,0 +1,50 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Reporte
+    {
+        public Dashboard verDashboard()
+        {
+            Dashboard objeto = new Dashboard();
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.conexion))
+                {
+                   
+                    SqlCommand cmd = new SqlCommand("sp_ReporteDashboard", oconexion);
+                    cmd.CommandType = CommandType.StoredProcedure;  
+                    oconexion.Open();
+
+                    // SqlDataReader hace la lectura y da el resultado de la ejecucion de la consulta
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            objeto = new Dashboard()
+                            {
+                                TotalCliente = Convert.ToInt32(reader["TotalCliente"]),
+                                TotalVenta = Convert.ToInt32(reader["TotalVenta"]),
+                                TotalProducto = Convert.ToInt32(reader["TotalPoducto"]),
+                            }; 
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                objeto = new Dashboard();
+            }
+
+            return objeto;
+        }
+    }
+}
