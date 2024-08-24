e6740d49b9e520092f118dc28d70a26ccac8a731
Jeim05 <lopezvegajeimmy@gmail.com>
2024-08-23 22:43:05 -0600

    Nuevos cambios

--- CapaPresentacionAdmin/Controllers/AccesoController.cs
+++ CapaPresentacionAdmin/Controllers/AccesoController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CapaEntidad;
+using CapaNegocio;
+
 namespace CapaPresentacionAdmin.Controllers
 {
     public class AccesoController : Controller
@@ -22,6 +25,28 @@ namespace CapaPresentacionAdmin.Controllers
         public ActionResult Reestablecer()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string correo, string clave)
+        {
+           Usuario oUsuario = new Usuario();
+            oUsuario = new CN_Usuarios().Listar().Where(u => u.Correo == correo && u.Clave == CN_Recursos.ConvertirSha256(clave)).FirstOrDefault();
+
+            if (oUsuario == null) {
+                ViewBag.Error = "Correo o contraseña incorrecta";
+                return View();
+            }
+            else
+            {
+                if (oUsuario.Reestablecer) {
+                    TempData["IdUsuario"] = oUsuario.IdUsuario;
+                    return RedirectToAction("CambiarClave");
+                }
+
+                ViewBag.Error = null;
+                return RedirectToAction("Index","Home");
+            }
         }
     }
 }
