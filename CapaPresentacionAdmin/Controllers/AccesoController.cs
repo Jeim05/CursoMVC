83adb164264a3ea11b0fe08c739f68c64f38d9f8
Jeim05 <lopezvegajeimmy@gmail.com>
2024-08-23 20:20:31 -0600

    Creacion de nuevo controlador para el login

---
+++ CapaPresentacionAdmin/Controllers/AccesoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentacionAdmin.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CambiarClave()
+        {
+            return View();
+        }
+
+        public ActionResult Reestablecer()
+        {
+            return View();
+        }
+    }
+}
