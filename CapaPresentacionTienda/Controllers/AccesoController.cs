a1429fa367d650afc349436479a360b3a0db7fee
Jeimmy López Vega <89615089+Jeim05@users.noreply.github.com>
2024-08-27 12:24:45 -0600

    Metodos para cliente

--- CapaPresentacionTienda/Controllers/AccesoController.cs
+++ CapaPresentacionTienda/Controllers/AccesoController.cs
@@ -33,7 +33,84 @@ namespace CapaPresentacionTienda.Controllers
 
         public ActionResult Registrar(Cliente objeto)
         {
+          int resultado;
+          string mensaje = string.Empty;
+
+          ViewData["Nombres"] = string.IsNullOrEmpty(objeto.Nombres) ? "" : objeto.Nombres;
+          ViewData["Apellidos"] = string.IsNullOrEmpty(objeto.Nombres) ? "" : objeto.Apellidos;
+          ViewData["Correo"] = string.IsNullOrEmpty(objeto.Nombres) ? "" : objeto.Correo;
+
+          if(objeto.Clave != objeto.ConfirmarClave){
+          ViewBag.Error = "Las contraseñas no coinciden";
+          return View();
+          }
+          resultado = new CN_Cliente().Registrar(objeto, out mensaje);
+
+          if(resultado > 0){
+            ViewBag.Error = null;
+            return RedirectToAction("Index","Acceso");
+          }else{
+           ViewBag.Error = mensaje;
+           return View();
+          }
+        }
+
+
+        [HttPost]
+        public ActionResult Index(string correo, string clave)
+        {
+         Cliente oCliente = null;
+         oCliente = new CN_Cliente().Listar().Where(item => item.Correo == correo && item.Clave == CN_Recursos.ConvertirSha256(clave)).FirstOrDefault();
+
+         if(oCliente){
+            ViewBag.Error = "Correo o contraseña incorrecta";
+           return View();
+         }else{
+           if(oCliente.Reestablecer){
+             TempData["IdCliente"] = oCliente.IdCliente;
+              return RedirectToAction("CambiarClave","Acceso");
+           }
+           else{
+           FormsAuthentication.SetAuthCookie(oCleinte.Correo, false);
+           Session["Cliente"]; = oCliente  // Guarda la información del cliente a través de todo el proyecto
+           ViewBag.Error = null;
+            return RedirectToAction("Index","Tienda");
+           }
+         }
+         
             return View();
         }
+
+
+        [HttPost]
+        public ActionResult Reestablecer()
+        {
+            Cliente oUsuario = new Cliente();
+           oCliente = new CN_Cliente().Listar().Where(item => item.Correo == correo).FirstOrDefault();
+
+       if(oCliente == null){
+       ViewBag.Error = "No se encontró un cliente relacionado a ese correo";
+        return View();
+       }
+
+        string mensaje = string.Empty;
+        bool respuesta = new CN_Cliente().ReestablecerClave(oCliente.IdCliente, correo, out mensaje);
+
+         if (respuesta) {
+       ViewBag.Error = null;
+        return RedirectToAction("Index", "Acceso");
+        }
+        else{
+         ViewBag.Error = mensaje;
+         return View();
+        }
+        }
+
+         [HttPost]
+        public ActionResult CambiarClave(string idcliente, string claveActual, string cleve)
+        {
+            return View();
+        }
+        
     }
 }
