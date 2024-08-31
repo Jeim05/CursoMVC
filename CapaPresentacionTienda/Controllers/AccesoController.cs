ab529a74b588592af87b1038b6e091de9bf403e9
Jeim05 <lopezvegajeimmy@gmail.com>
2024-08-30 20:34:20 -0600

    CERRAR SESION Y BOTONES

--- CapaPresentacionTienda/Controllers/AccesoController.cs
+++ CapaPresentacionTienda/Controllers/AccesoController.cs
@@ -159,6 +159,7 @@ namespace CapaPresentacionTienda.Controllers
 
         public ActionResult CerrarSesion()
         {
+            Session["Cliente"] = null;
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Acceso");
         }
