3ffc991cb28e96085c89ed27bff22a338a944d25
Jeim05 <lopezvegajeimmy@gmail.com>
2024-09-03 22:28:24 -0600

    Metodo para verificar si existe el carrito

---
+++ CapaDatos/CD_Carrito.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class CD_Carrito
+    {
+        public bool ExisteCarrito(int idcliente, int idproducto)
+        {
+            bool resultado = true;
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.conexion))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_ExisteCarrito", conexion); 
+                    cmd.Parameters.AddWithValue("IdCliente", idcliente);
+                    cmd.Parameters.AddWithValue("IdProducto", idproducto);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output; 
+                    cmd.CommandType = CommandType.StoredProcedure; 
+
+                    conexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value); 
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+            }
+            return resultado;
+        }
+    }
+}
