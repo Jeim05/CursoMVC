1e29153dc86f375afe4d533f600e3b733e1b35c1
Jeimmy López Vega <89615089+Jeim05@users.noreply.github.com>
2024-09-10 11:51:12 -0600

    Metodo en capa Negocio Registrar Venta

--- CapaNegocio/CN_Venta.cs
+++ CapaNegocio/CN_Venta.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CapaDatos;
+using CapaEntidad;
+
 namespace CapaNegocio
 {
     public class CN_Venta
     {
+         private CD_Venta objCapaDatos = new CD_Venta();
+
+          public bool Registrar(Venta obj, DataTable DetalleVenta, out string Mensaje){
+            return objCapaDatos.Registrar(obj, DetalleVenta, Mensaje)
+          }
+        
     }
 }
